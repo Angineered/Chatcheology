f6ba51315d0550a7c04cd11c3a11b10cdf656162
Angelique Henriques-Fernandes <info@angineered.co.za>
2026-08-17 14:44:57 +0200

    Initial Chatcheology solution

---
+++ Chatcheology.Core.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace Chatcheology.Core.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Chatcheology.Core/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Chatcheology.Core
+{
+    public class Class1
+    {
+
+    }
+}
